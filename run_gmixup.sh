0e4b4d93393329dadf4f850c53e6309e734b6cb3
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2024-04-16 17:23:04 +0530

    Update run_gmixup.sh

--- run_gmixup.sh
+++ run_gmixup.sh
@@ -1,7 +1,34 @@
+#!/bin/bash
 
-for seed in 1314 11314 21314 31314 41314 51314 61314 71314
-do 
-    CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset REDDIT-BINARY \
-        --lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
-        --aug_ratio 0.15 --aug_num 10  --ge USVT
-done
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset PTC_MR \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat
+
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset PROTEINS \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat
+
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset IMDB-BINARY \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat
+
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset NCI1 \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat
+
+
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset PTC_MR \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat --tail_aug
+
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset PROTEINS \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat --tail_aug
+
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset IMDB-BINARY \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat --tail_aug
+
+CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset NCI1 \
+--lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+--aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat --tail_aug
