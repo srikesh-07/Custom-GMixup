2648a3dfb5aef24819ae8b306f44933da0cbcff8
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2024-04-16 19:44:40 +0530

    Update run_gmixup.sh

--- run_gmixup.sh
+++ run_gmixup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+seed=0
+
 python -u ./src/gmixup.py --data_path . --model GIN --dataset PTC_MR \
 --lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
 --aug_ratio 0.15 --aug_num 10  --ge USVT --size_strat
